c79d720ce4b6bd7deaea2e4a7d2282cfe4104a1d
Alexandr <alexcist33@gmail.com>
2025-01-26 17:45:50 +0300

    Добавил логирование в файл main и config для работы с токеном бота

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+TOKEN_BOT = os.environ.get("TOKEN_BOT")
---
+++ main.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+
+from pythonjsonlogger import jsonlogger
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters.command import Command
+
+from config import TOKEN_BOT
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+# настроем логирование в файл в формате JSON
+logHandler = logging.FileHandler('Logs_bot/tortaletka_bot.log')
+formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(message)s')
+logHandler.setFormatter(formatter)
+# Объект бота
+bot = Bot(TOKEN_BOT, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+# Диспетчер
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
