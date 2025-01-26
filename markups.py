518533cdeb65845c1c4c783a350a79f06ecc51e9
Alexandr <alexcist33@gmail.com>
2025-01-26 20:22:26 +0300

    создал отдельный файл для кнопок markups,

---
+++ markups.py
@@ -0,0 +1,5 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import URL_CHANNEL
+btn_url_channel = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url=URL_CHANNEL)
+inline_kb_list = [[btn_url_channel]]
+check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
