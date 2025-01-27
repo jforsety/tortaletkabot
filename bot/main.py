c8014ab43fca5f70eec521dbb8f671d578f1b802
Alexandr <alexcist33@gmail.com>
2025-01-28 00:34:44 +0300

    Добавил команду manual

--- bot/main.py
+++ bot/main.py
@@ -9,7 +9,7 @@ from aiogram.filters.command import Command
 
 from bot.config import TOKEN_BOT, CHANNEL_ID
 from bot.markups import check_sub_menu
-from bot.text_bot.text import start_text, not_sub_message
+from bot.text_bot.text import start_text, not_sub_message, manual_text
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -39,6 +39,16 @@ async def cmd_start(message: types.Message):
     if message.chat.type == 'private':
         if check_sub_channel(await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=message.from_user.id)):
             await message.answer(start_text)
+        else:
+            await message.answer(not_sub_message, reply_markup=check_sub_menu)
+
+
+# Хэндлер на команду /manual
+@dp.message(Command("manual"))
+async def cmd_manual(message: types.Message):
+    if message.chat.type == 'private':
+        if check_sub_channel(await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=message.from_user.id)):
+            await message.answer(manual_text)
         else:
             await message.answer(not_sub_message, reply_markup=check_sub_menu)
 
--- bot/markups.py
+++ bot/markups.py
@@ -4,3 +4,6 @@ from config import URL_CHANNEL
 btn_url_channel = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url=URL_CHANNEL)
 inline_kb_list = [[btn_url_channel]]
 check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+
+
+
