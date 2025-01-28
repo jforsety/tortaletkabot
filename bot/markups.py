45dfd5bf2907520c96b906c3970e82214ed1971d
Alexandr <alexcist33@gmail.com>
2025-01-28 22:38:06 +0300

    Добавил кнопку Пригласи друга

--- bot/markups.py
+++ bot/markups.py
@@ -1,9 +1,12 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from config import URL_CHANNEL
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+from config import URL_CHANNEL, URL_INVITE
 
 btn_url_channel = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url=URL_CHANNEL)
-inline_kb_list = [[btn_url_channel]]
-check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+inline_start = [[btn_url_channel]]
+check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_start)
+
+btn_invite = InlineKeyboardButton(text="📤Пригласить друга", url=URL_INVITE)
+
 
 
 
