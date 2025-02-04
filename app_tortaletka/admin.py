a08db971e5605568558ddcc614b40c483821f2d3
Alexandr <alexcist33@gmail.com>
2025-02-05 00:58:12 +0300

    Отключение отображения дефолтных групп в админке

--- app_tortaletka/admin.py
+++ app_tortaletka/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
 from .models import Client
 
@@ -8,3 +9,7 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ("external_id", "first_name","username", "attempt", "last_date")
     list_filter = ("first_name","username", "attempt")
     fields = ("first_name","username", "attempt")
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
