d42867d7365c35b48399838a30202744bc9a3535
Alexandr <alexcist33@gmail.com>
2025-02-05 00:35:55 +0300

    Доработка модели Client

--- app_tortaletka/models.py
+++ app_tortaletka/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Client(models.Model):
-    external_id = models.PositiveIntegerField(verbose_name='ID пользователя в соц сети', unique=True)
-    first_name = models.CharField(max_length=30, verbose_name='Имя')
-    last_name = models.CharField(max_length=30, verbose_name='Фамилия')
-    username = models.CharField(max_length=30, verbose_name='nickname')
+    external_id = models.PositiveIntegerField(verbose_name='ID', unique=True)
+    first_name = models.CharField(max_length=30, verbose_name='Имя', null=True)
+    last_name = models.CharField(max_length=30, verbose_name='Фамилия', null=True)
+    username = models.CharField(max_length=30, verbose_name='nickname', null=True)
     attempt = models.IntegerField(default=10, verbose_name='Попытки')
     premium_status = models.BooleanField(default=0, verbose_name='Премиум статус')
     referrals = models.IntegerField(default=0, verbose_name='Рефералы')
