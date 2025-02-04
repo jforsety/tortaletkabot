64f4efe490b1ef7e854c3a4ea6f4c5ecd0ca536b
Alexandr <alexcist33@gmail.com>
2025-02-05 00:33:05 +0300

    Создание отдельного файла для работы с БД, реализация функции add_user

---
+++ app_bot/database.py
@@ -0,0 +1,23 @@
+import datetime
+import sqlite3
+
+database = sqlite3.connect("db.sqlite3")
+cursor = database.cursor()
+
+
+def add_user(message):
+    cursor.execute("SELECT external_id FROM app_tortaletka_client WHERE external_id=?", (message.from_user.id,))
+    user = cursor.fetchone()
+    if not user:
+        cursor.execute("INSERT INTO app_tortaletka_client (external_id, first_name, last_name, attempt, "
+                       "premium_status, referrals, date_registration, last_date, username) VALUES(?,?,?,?,?,?,?,?,"
+                       "?)", (message.from_user.id,
+                                        message.from_user.first_name,
+                                        message.from_user.last_name,
+                                        30,0,0,
+                                        datetime.datetime.now(),
+                                        datetime.datetime.now(),
+                                        message.from_user.username))
+    else:
+        cursor.execute("UPDATE app_tortaletka_client SET last_date=? WHERE external_id=?", (datetime.datetime.now(), user[0]))
+    database.commit()
