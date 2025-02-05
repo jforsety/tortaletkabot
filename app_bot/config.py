8514c93f088dae560348d1ecec49ff9842330905
Alexandr <alexcist33@gmail.com>
2025-02-05 22:56:04 +0300

    Корректировка конфига

--- app_bot/config.py
+++ app_bot/config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv("../.env")
 
-TOKEN_BOT = os.environ.get("TOKEN_BOT")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
 URL_CHANNEL = os.environ.get("URL_CHANNEL")
 URL_INVITE = os.environ.get("URL_INVITE")
 SECRET_KEY_DJANGO = os.environ.get("SECRET_KEY_DJANGO")
