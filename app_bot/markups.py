e211cde8a93cb6acec1c6fef68028d619e4093d1
Alexandr <alexcist33@gmail.com>
2025-02-06 02:12:31 +0300

    Добавил функцию get_referral_keyboard? генерация реферальной ссылки

--- app_bot/markups.py
+++ app_bot/markups.py
@@ -1,6 +1,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from app_bot.config import URL_CHANNEL, URL_INVITE
 
+
 btn_url_channel = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url=URL_CHANNEL)
 inline_start = [[btn_url_channel]]
 check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_start)
@@ -8,6 +9,15 @@ check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_start)
 btn_invite = InlineKeyboardButton(text="📤Пригласить друга", url=URL_INVITE)
 btn_instruction_list = [[btn_invite]]
 instruction_btn = InlineKeyboardMarkup(inline_keyboard=btn_instruction_list)
+
+
+def get_referral_keyboard(user_id: int, bot_url: str):
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="📤Пригласить друга",
+                                 url=f"https://t.me/share/url?url={bot_url}?start={user_id}")
+        ]
+    ])
 
 
 
