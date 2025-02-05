789369234a083f9cacc9fd760c8a140da6b5a189
Alexandr <alexcist33@gmail.com>
2025-02-05 23:28:03 +0300

    Реализовал функцию profile_exists и  profile_attempts для получения данных для профиля

--- app_bot/database.py
+++ app_bot/database.py
@@ -6,18 +6,34 @@ cursor = database.cursor()
 
 
 def add_user(message):
+    """Регистрация пользователя"""
     cursor.execute("SELECT external_id FROM app_tortaletka_client WHERE external_id=?", (message.from_user.id,))
     user = cursor.fetchone()
     if not user:
         cursor.execute("INSERT INTO app_tortaletka_client (external_id, first_name, last_name, attempt, "
                        "premium_status, referrals, date_registration, last_date, username) VALUES(?,?,?,?,?,?,?,?,"
                        "?)", (message.from_user.id,
-                                        message.from_user.first_name,
-                                        message.from_user.last_name,
-                                        30,0,0,
-                                        datetime.datetime.now(),
-                                        datetime.datetime.now(),
-                                        message.from_user.username))
+                              message.from_user.first_name,
+                              message.from_user.last_name,
+                              30, 0, 0,
+                              datetime.datetime.now(),
+                              datetime.datetime.now(),
+                              message.from_user.username))
     else:
-        cursor.execute("UPDATE app_tortaletka_client SET last_date=? WHERE external_id=?", (datetime.datetime.now(), user[0]))
+        cursor.execute("UPDATE app_tortaletka_client SET last_date=? WHERE external_id=?",
+                       (datetime.datetime.now(), user[0]))
     database.commit()
+
+
+def profile_exists(message):
+    """Получение информации по профилю"""
+    result = cursor.execute("SELECT * FROM `app_tortaletka_client` WHERE `external_id` = ?",
+                            (message.from_user.id,)).fetchall()
+    return result
+
+
+def profile_attempts(message):
+    """Получение попыток профиля"""
+    result = cursor.execute("SELECT `attempt` FROM `app_tortaletka_client` WHERE `external_id` = ?",
+                            (message.from_user.id,)).fetchone()
+    return result
