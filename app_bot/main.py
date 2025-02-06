c41ebd35af88a8f85fce29d8fc4db406c48c7c0d
Alexandr <alexcist33@gmail.com>
2025-02-06 11:46:24 +0300

    Добавление в команду старт проверку для рефералов, добавление кнопки пригласить в команду инструкций

--- app_bot/main.py
+++ app_bot/main.py
@@ -8,7 +8,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters.command import Command
 
-from app_bot.database import add_user, profile_exists
+from app_bot.database import add_user, profile_exists, referral_reg
 from app_bot.markups import check_sub_menu, instruction_btn, get_referral_keyboard
 from app_bot.text_bot.text import start_text, not_sub_message, instruction_text
 
@@ -36,9 +36,14 @@ def check_sub_channel(chat_member):
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
+    referral_id = None
     if message.chat.type == 'private':
         if check_sub_channel(await bot.get_chat_member(chat_id=os.environ.get("CHANNEL_ID"), user_id=message.from_user.id)):
             add_user(message)
+            if len(message.text.split()) > 1:
+                referral_id = message.text.split()[1].replace("ref_", "")
+            if referral_id and referral_id.isdigit():
+                referral_reg(message,referral_id)
             await message.answer(start_text)
         else:
             await message.answer(not_sub_message, reply_markup=check_sub_menu)
@@ -47,9 +52,13 @@ async def cmd_start(message: types.Message):
 # Хэндлер на команду /instruction
 @dp.message(Command("instruction"))
 async def cmd_instruction(message: types.Message):
+    keyboard = get_referral_keyboard(
+        user_id=message.from_user.id,
+        bot_url=os.environ.get("URL_BOT")
+    )
     if message.chat.type == 'private':
         if check_sub_channel(await bot.get_chat_member(chat_id=os.environ.get("CHANNEL_ID"), user_id=message.from_user.id)):
-            await message.answer(instruction_text, reply_markup=instruction_btn)
+            await message.answer(instruction_text, reply_markup=keyboard)
         else:
             await message.answer(not_sub_message, reply_markup=check_sub_menu)
 
@@ -64,13 +73,13 @@ async def cmd_profile(message: types.Message):
     if message.chat.type == 'private':
         if check_sub_channel(await bot.get_chat_member(chat_id=os.environ.get("CHANNEL_ID"), user_id=message.from_user.id)):
             profile_list = profile_exists(message)
-            await message.answer(text=f"<b>👤Профиль пользователя:</b>\n\n"
+            await message.answer(text=f"<b>👤 Профиль пользователя:</b>\n\n"
                                       f"<b>ID:</b> {profile_list[0][1]}\n"
                                       f"<b>Имя:</b> {profile_list[0][2]}\n"
                                       f"<b>Рефералы:</b> {profile_list[0][6]}\n"
                                       f"<b>Подписка:</b> {profile_list[0][5]}\n"
                                       f"<b>Попытки:</b> {profile_list[0][4]}\n"
-                                      f"<i>*За каждого приглашенного друга"
+                                      f"<i>*За каждого приглашенного друга "
                                       f"даётся 5 бесплатных запросов*</i>", reply_markup=keyboard)
         else:
             await message.answer(not_sub_message, reply_markup=check_sub_menu)
