f7498790d5a48fca60860c3b30301019df0c31bf
Alexandr <alexcist33@gmail.com>
2025-02-10 12:58:02 +0300

    Убрал конфиг

--- app_bot/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv("../.env")
-
-URL_CHANNEL = os.environ.get("URL_CHANNEL")
-URL_INVITE = os.environ.get("URL_INVITE")
-SECRET_KEY_DJANGO = os.environ.get("SECRET_KEY_DJANGO")
