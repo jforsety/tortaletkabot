02db7ef47f348383833c256b5dc3bdcec5e468b4
Alexandr <alexcist33@gmail.com>
2025-02-15 00:46:23 +0300

    Создание кнопок для команды admin

--- app_bot/keyboards.py
+++ app_bot/keyboards.py
@@ -3,11 +3,17 @@ import os
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 btn_url_channel = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url=os.getenv('CHANNEL_URL'))
 inline_start = [[btn_url_channel]]
 check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_start)
+
+btn_update_attempts = InlineKeyboardButton(text="ОБНОВИТЬ ПОПЫТКИ (ВСЕ)", callback_data="update_1")
+btn_update_admin = InlineKeyboardButton(text="ОБНОВИТЬ ПОПЫТКИ (АДМ)", callback_data="update_2")
+inline_admin = [[btn_update_attempts],[btn_update_admin]]
+admin_btn = InlineKeyboardMarkup(inline_keyboard=inline_admin)
 
 
 def get_referral_keyboard(user_id: int, bot_url: str):
