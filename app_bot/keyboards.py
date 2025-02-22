f7e3b5fc5b2d793014d335b4740ba10dbaa7a450
Alexandr <alexcist33@gmail.com>
2025-02-22 20:18:34 +0300

    Добавил описание и символическое разделение клавиатур

--- app_bot/keyboards.py
+++ app_bot/keyboards.py
@@ -6,16 +6,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Клавиатура кнопки подписаться
 btn_url_channel = InlineKeyboardButton(text="ПОДПИСАТЬСЯ", url=os.getenv('CHANNEL_URL'))
 inline_start = [[btn_url_channel]]
 check_sub_menu = InlineKeyboardMarkup(inline_keyboard=inline_start)
 
+# Клавиатура админки в боте
 btn_update_attempts = InlineKeyboardButton(text="ОБНОВИТЬ ПОПЫТКИ (ВСЕ)", callback_data="update_1")
 btn_update_admin = InlineKeyboardButton(text="ОБНОВИТЬ ПОПЫТКИ (АДМ)", callback_data="update_2")
-inline_admin = [[btn_update_attempts],[btn_update_admin]]
+inline_admin = [[btn_update_attempts], [btn_update_admin]]
 admin_btn = InlineKeyboardMarkup(inline_keyboard=inline_admin)
 
 
+# Функция клавиатуры отправления реферальной ссылки
 def get_referral_keyboard(user_id: int, bot_url: str):
     return InlineKeyboardMarkup(inline_keyboard=[
         [
