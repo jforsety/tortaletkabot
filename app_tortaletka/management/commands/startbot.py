0792691bfd5d85f9cf57662a8334d12c591627bf
Alexandr <alexcist33@gmail.com>
2025-02-22 20:22:37 +0300

    Изменение команды startbot для запуска асинхронно бота и планировщика

--- app_tortaletka/management/commands/startbot.py
+++ app_tortaletka/management/commands/startbot.py
@@ -2,14 +2,23 @@ import asyncio
 
 from django.core.management.base import BaseCommand
 
-from app_bot.main import bot, dp, logger
+from app_bot.main import bot, dp, scheduler
 
 
 class Command(BaseCommand):
     help = "Запуск телеграм бота"
 
     def handle(self, *args, **options):
-        logger.info("Бот был запущен")
-        asyncio.run(dp.start_polling(bot))
-        logger.info("Бот был остановлен")
+        # Запуск асинхронного контекста с ботом и планировщиком
+        async def main():
+            # Запуск планировщика
+            scheduler.start()
 
+            # Запуск бота
+            await dp.start_polling(bot)
+
+            # Бесконечный цикл для работы планировщика
+            while True:
+                await asyncio.sleep(3600)  # Просто поддерживаем цикл
+
+        asyncio.run(main())
