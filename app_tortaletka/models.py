c1e9bdbb5e4f266bad0542567053de3395b396a8
Alexandr <alexcist33@gmail.com>
2025-02-25 23:43:55 +0300

    Реализация класса BroadcastMessage

--- app_tortaletka/models.py
+++ app_tortaletka/models.py
@@ -19,3 +19,44 @@ class Client(models.Model):
 
     def __str__(self):
         return f'{self.id} : {self.first_name}'
+
+
+class BroadcastMessage(models.Model):
+    CONTENT_TYPE_CHOICES = (
+        ('text', 'Только текст'),
+        ('image', 'Только изображение'),
+        ('both', 'Текст + изображение'),
+    )
+
+    content_type = models.CharField(
+        max_length=5,
+        choices=CONTENT_TYPE_CHOICES,
+        default='text',
+        verbose_name="Тип контента"
+    )
+    text = models.TextField(
+        verbose_name="Текст сообщения",
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        upload_to='broadcast/',
+        verbose_name="Изображение",
+        null=True,
+        blank=True
+    )
+    is_broadcast = models.BooleanField(
+        default=False,
+        verbose_name="Всем пользователям"
+    )
+    users = models.ManyToManyField(
+        Client,
+        blank=True,
+        verbose_name="Конкретные пользователи"
+    )
+    sent = models.BooleanField(default=False, verbose_name="Отправлено")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
