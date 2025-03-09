f35fa1673560665b56e87268d0d921961e47ea7a
Alexandr <alexcist33@gmail.com>
2025-03-09 20:40:10 +0300

    Изменение модели Client

--- app_tortaletka/models.py
+++ app_tortaletka/models.py
@@ -7,7 +7,7 @@ class Client(models.Model):
     last_name = models.CharField(max_length=30, verbose_name='Фамилия', null=True)
     username = models.CharField(max_length=30, verbose_name='nickname', null=True)
     attempt = models.IntegerField(default=10, verbose_name='Попытки')
-    premium_status = models.BooleanField(default=0, verbose_name='Премиум статус')
+    premium_status = models.BooleanField(default=False, verbose_name='Премиум статус')
     referrals = models.IntegerField(default=0, verbose_name='Рефералы')
     date_registration = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
     last_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата активности')
