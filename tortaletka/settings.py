cc52f3d9c8fe32c71f8b4cb1d8860dc38f20b1ff
Alexandr <alexcist33@gmail.com>
2025-03-09 16:59:28 +0300

    Удалил views, добавлена иконка для сайта. Также добавил настройки в url и settings (Установлена кастомная админка django-admin-interface )

--- tortaletka/settings.py
+++ tortaletka/settings.py
@@ -34,6 +34,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
